1607a7dfe1c1e620b6da4178574d0cee19dd1aaa
wmostom <31738874+wmostom@users.noreply.github.com>
2018-01-11 10:21:12 -0600

    Add files via upload

---
+++ euler1improvment.py
@@ -0,0 +1,4 @@
+print sum(x for x in range(0,1000)
+#this makes it so it looks for the numbers between 1-1000
+if (x % 3 == 0 or x % 5 == 0))
+# this takes the numbers between 1-1000 that are divisable by 3 and 5. the == 0 means it cant have a remainder 
